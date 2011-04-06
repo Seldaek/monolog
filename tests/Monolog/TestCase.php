a390331a5d4e7e62bb878a5c83166bada2044f67
Jordi Boggiano <j.boggiano@seld.be>
2011-04-06 13:56:47 +0200

    Copy paste detected :)

--- tests/Monolog/TestCase.php
+++ tests/Monolog/TestCase.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the Monolog package.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.

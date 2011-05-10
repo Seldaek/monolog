0eb5896c5bd4657f484200c3f392ca4155b78669
lenar <lenar@city.ee>
2011-05-10 18:04:41 +0300

    Rename to GroupHandler, remove callback support, add couple of tests

---
+++ tests/Monolog/Handler/GroupHandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Handler;
+
+use Monolog\TestCase;
+use Monolog\Logger;
+
+class ForwarderHandlerTest extends TestCase
+{
+    public function testHandleForward()
+    {
+        $testHandlers = array(new TestHandler(), new TestHandler());
+        $handler = new ForwarderHandler($testHandlers);
+        $handler->handle($this->getRecord(Logger::DEBUG));
+        $handler->handle($this->getRecord(Logger::INFO));
+        foreach ($testHandlers as $test) {
+            $this->assertTrue($test->hasDebugRecords());
+            $this->assertTrue($test->hasInfoRecords());
+            $this->assertTrue(count($test->getRecords()) === 2);
+        }
+    }
+
+    public function testHandleBatchForward()
+    {
+        $testHandlers = array(new TestHandler(), new TestHandler());
+        $handler = new ForwarderHandler($testHandlers);
+        $handler->handleBatch(array($this->getRecord(Logger::DEBUG), $this->getRecord(Logger::INFO)));
+        foreach ($testHandlers as $test) {
+            $this->assertTrue($test->hasDebugRecords());
+            $this->assertTrue($test->hasInfoRecords());
+            $this->assertTrue(count($test->getRecords()) === 2);
+        }
+    }
+}

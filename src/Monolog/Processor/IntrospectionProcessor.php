7e0f4ef70e6f66326b4899d1929ed603f9373f2f
Jordi Boggiano <j.boggiano@seld.be>
2011-07-04 22:16:42 +0200

    Add word of caution to the IntrospectionProcessor

--- src/Monolog/Processor/IntrospectionProcessor.php
+++ src/Monolog/Processor/IntrospectionProcessor.php
@@ -14,6 +14,12 @@ namespace Monolog\Processor;
 /**
  * Injects line/file:class/function where the log message came from
  *
+ * Warning: This only works if the handler processes the logs directly.
+ * If you put the processor on a handler that is behind a FingersCrossedHandler
+ * for example, the processor will only be called once the trigger level is reached,
+ * and all the log records will have the same file/line/.. data from the call that
+ * triggered the FingersCrossedHandler.
+ *
  * @author Jordi Boggiano <j.boggiano@seld.be>
  */
 class IntrospectionProcessor

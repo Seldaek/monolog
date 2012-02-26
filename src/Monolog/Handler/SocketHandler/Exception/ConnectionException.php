423cfebf94df25f993978e76e5300b1bafbd55a7
Pablo Belloc <pablo.belloc@fnbox.com>
2012-02-25 23:36:07 -0300

    Add license header and move @author to phpdoc

--- src/Monolog/Handler/SocketHandler/Exception/ConnectionException.php
+++ src/Monolog/Handler/SocketHandler/Exception/ConnectionException.php
@@ -1,6 +1,12 @@
 <?php
-/**
- * @author Pablo de Leon Belloc <pablolb@gmail.com>
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Monolog\Handler\SocketHandler\Exception;
--- tests/Monolog/Handler/SocketHandlerTest.php
+++ tests/Monolog/Handler/SocketHandlerTest.php
@@ -1,8 +1,13 @@
 <?php
-/**
- * @author Pablo de Leon Belloc <pablolb@gmail.com>
- */
 
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Monolog\Handler;
 
@@ -13,6 +18,11 @@ use Monolog\Handler\SocketHandler\PersistentSocket;
 use Monolog\TestCase;
 use Monolog\Logger;
 
+
+
+/**
+ * @author Pablo de Leon Belloc <pablolb@gmail.com>
+ */
 class SocketHandlerTest extends TestCase
 {
     public function testWrite()

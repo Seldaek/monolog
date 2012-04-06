e882c3586fbe5b3ab3b15ce1f0bc264d4139a0c2
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-05 22:40:07 -0700

    Make some changes on top of @mlehner's changes suggested by @stof in https://github.com/Seldaek/monolog/pull/61
    I modified composer.json to point to my fork of gelf-php
    (https://github.com/msabramo/gelf-php) for the time being, as my fork
    has a composer.json, namespaces, and the IMessagePublisher interface.
    I've also submitted pull requests with my changes upstream to
    https://github.com/Graylog2/gelf-php. If those changes are accepted,
    then we can use the official repo or if they register the package on
    Packagist (which I suggested in one of my requests), then we can just
    eliminate the repository altogether.

--- src/Monolog/Handler/GelfHandler.php
+++ src/Monolog/Handler/GelfHandler.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace Tellecore\Monolog\Handler;
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Handler;
 
 use Gelf\Message;
-use Gelf\MessagePublisher;
-
-use Monolog\Formatter\SimpleFormatter;
+use Gelf\IMessagePublisher;
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
 
@@ -13,28 +20,29 @@ use Monolog\Handler\AbstractProcessingHandler;
  * Handler to send messages to a Graylog2 (http://www.graylog2.org) server 
  *
  * @author Matt Lehner <mlehner@gmail.com>
+ * @author Marc Abramowitz <marc@marc-abramowitz.com>
  */
 class GelfHandler extends AbstractProcessingHandler
 {
-    /*
-     * @var Gelf\MessagePublisher the publisher object that sends the message to the server
+    /**
+     * @var Gelf\IMessagePublisher the publisher object that sends the message to the server
      */
     protected $publisher;
 
-    /*
+    /**
      * @var string the name of the system for the Gelf log message
      */
-    protected $system_name;
+    protected $systemName;
 
-    /*
+    /**
      * @var string a prefix for 'extra' fields from the Monolog record (optional)
      */
-    protected $extra_prefix;
+    protected $extraPrefix;
 
-    /*
+    /**
      * @var string a prefix for 'context' fields from the Monolog record (optional)
      */
-    protected $context_prefix;
+    protected $contentPrefix;
 
     /**
      * Translates Monolog log levels to Graylog2 log priorities.
@@ -49,22 +57,23 @@ class GelfHandler extends AbstractProcessingHandler
     );
 
     /**
-     * @param Gelf\MessagePublisher $publisher a publisher object
-     * @param string $system_name the name of the system sending messages
+     * @param Gelf\IMessagePublisher $publisher a publisher object
+     * @param string $systemName the name of the system sending messages
      * @param integer $level The minimum logging level at which this handler will be triggered
      * @param Boolean $bubble Whether the messages that are handled can bubble up the stack or not
-     * @param string $extra_prefix a string to prefix for all the 'extra' fields from Monolog record
-     * @oaram string $context_prefix a string to prefix for all the 'context' fields from a Monolog record
+     * @param string $extraPrefix a string to prefix for all the 'extra' fields from Monolog record
+     * @oaram string $contentPrefix a string to prefix for all the 'context' fields from a Monolog record
      */
-    public function __construct(MessagePublisher $publisher, $system_name = null, $level = Logger::DEBUG, $bubble = true, $extra_prefix = null, $context_prefix = null)
+    public function __construct(IMessagePublisher $publisher, $systemName = null, $level = Logger::DEBUG,
+                                $bubble = true, $extraPrefix = null, $contentPrefix = 'ctxt_')
     {
         parent::__construct($level, $bubble);
 
         $this->publisher = $publisher;
-        $this->system_name = $system_name ?: gethostname();
+        $this->systemName = $systemName ?: gethostname();
 
-        $this->extra_prefix = $extra_prefix;
-        $this->context_prefix = $context_prefix;
+        $this->extraPrefix = $extraPrefix;
+        $this->contentPrefix = $contentPrefix;
     }
 
     /**
@@ -86,7 +95,7 @@ class GelfHandler extends AbstractProcessingHandler
             ->setShortMessage((string) $record['message'])
             ->setFullMessage((string) $record['formatted'])
             ->setFacility($record['channel'])
-            ->setHost($this->system_name)
+            ->setHost($this->systemName)
             ->setLine(isset($record['extra']['line']) ? $record['extra']['line'] : null)
             ->setFile(isset($record['extra']['file']) ? $record['extra']['file'] : null)
             ->setLevel($this->logLevels[ $record['level'] ]);
@@ -95,14 +104,12 @@ class GelfHandler extends AbstractProcessingHandler
         unset($record['extra']['line']);
         unset($record['extra']['file']);
 
-        foreach ($record['extra'] as $key => $val)
-        {
-            $message->setAdditional($this->extra_prefix . $key, is_scalar($val) ? $val : json_encode($val));
+        foreach ($record['extra'] as $key => $val) {
+            $message->setAdditional($this->extraPrefix . $key, is_scalar($val) ? $val : json_encode($val));
         }
 
-        foreach ($record['context'] as $key => $val)
-        {
-            $message->setAdditional($this->context_prefix . $key, is_scalar($val) ? $val : json_encode($val));
+        foreach ($record['context'] as $key => $val) {
+            $message->setAdditional($this->contentPrefix . $key, is_scalar($val) ? $val : json_encode($val));
         }
 
         $this->publisher->publish($message);

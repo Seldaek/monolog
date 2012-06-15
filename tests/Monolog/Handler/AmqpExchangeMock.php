e5ffccd4221f91f3d7c32075f08d98cb60a59dc1
pomaxa <pomaxa@gmail.com>
2012-06-15 17:56:19 +0300

    fix indentation

--- tests/Monolog/Handler/AmqpExchangeMock.php
+++ tests/Monolog/Handler/AmqpExchangeMock.php
@@ -8,21 +8,22 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Monolog\Handler;
 
 class MockAMQPExchange extends \AMQPExchange
 {
-public function __construct()
-{
-}
+    public function __construct()
+    {
+    }
 
-public function publish($message, $routing_key, $params = 0, $attributes = array())
-{
-return true;
-}
+    public function publish($message, $routing_key, $params = 0, $attributes = array())
+    {
+        return true;
+    }
 
-public function setName($name)
-{
-return true;
-}
+    public function setName($name)
+    {
+        return true;
+    }
 }

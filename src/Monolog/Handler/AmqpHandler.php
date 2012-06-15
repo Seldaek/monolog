0932fd33c4d8c6866a7041b235cfc80352964077
pomaxa <pomaxa@gmail.com>
2012-06-15 14:31:50 +0300

    comments & readme update

--- src/Monolog/Handler/AmqpHandler.php
+++ src/Monolog/Handler/AmqpHandler.php
@@ -37,10 +37,7 @@ class AmqpHandler extends AbstractProcessingHandler
     }
 
     /**
-     * Writes the record down to the log of the implementing handler
-     *
-     * @param array $record
-     * @return void
+     * {@inheritDoc}
      */
     protected function write(array $record)
     {

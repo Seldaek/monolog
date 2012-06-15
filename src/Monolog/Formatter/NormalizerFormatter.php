4829b09743c9708cb9351a4c4dfe8eae5ba422e3
Jordi Boggiano <j.boggiano@seld.be>
2012-06-15 16:45:18 +0200

    Avoid failures when normalizing exotic values, fixes #89

--- src/Monolog/Formatter/NormalizerFormatter.php
+++ src/Monolog/Formatter/NormalizerFormatter.php
@@ -70,11 +70,15 @@ class NormalizerFormatter implements FormatterInterface
             return $data->format($this->dateFormat);
         }
 
+        if (is_object($data)) {
+            return sprintf("[object] (%s: %s)", get_class($data), $this->toJson($data));
+        }
+
         if (is_resource($data)) {
             return '[resource]';
         }
 
-        return sprintf("[object] (%s: %s)", get_class($data), $this->toJson($data));
+        return '[unknown('.gettype($data).')]';
     }
 
     protected function toJson($data)

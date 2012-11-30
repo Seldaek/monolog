473a7e44bc9e59f3f0c6d791dd18a9cc90f10d20
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-11-30 13:55:57 +0100

    Started on notes from Pascal and Christophe

--- src/Monolog/Handler/PusherHandler.php
+++ src/Monolog/Handler/PusherHandler.php
@@ -3,8 +3,12 @@
 /*
  * This file is part of the Monolog package.
  *
- * (c) Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
  *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Ross Crawford-d'Heureuse <sendrossemail+monolog@gmail.com>
  */
 
 namespace Monolog\Handler;
@@ -19,31 +23,28 @@ class PusherHandler extends AbstractProcessingHandler
     /**
      * @var internal variables
      */
-    protected
-        $pusher_key, 
-        $pusher_secret, 
-        $pusher_app_id, 
-        $channel, 
-        $event;
-
-    private 
-        $pusher;
+    protected $pusherKey;
+    protected $pusherSecret;
+    protected $pusherApp_id; 
+    protected $channel;
+    protected $event;
+    protected $pusher;
 
     /**
-     * @param string        $pusher_key
-     * @param string        $pusher_secret
-     * @param string        $pusher_app_id
+     * @param string        $pusherKey
+     * @param string        $pusherSecret
+     * @param string        $pusherAppId
      * @param string        $channel
-     * @param string        $channel
+     * @param string        $event
      * @param int           $level
      * @param bool          $bubble       Whether the messages that are handled can bubble up the stack or not
      */
-    public function __construct($pusher_key, $pusher_secret, $pusher_app_id, $channel, $event='info_log', $level = Logger::INFO, $bubble = true)
+    public function __construct($pusherKey, $pusherSecret, $pusherAppId, $channel, $event='info_log', $level = Logger::INFO, $bubble = true)
     {
         $this->channel = $channel;
         $this->event = $event;
-        //$this->pusher = new Pusher($pusher_key, $pusher_secret, $pusher_app_id, true);// DEBUG
-        $this->pusher = new Pusher($pusher_key, $pusher_secret, $pusher_app_id);
+
+        $this->pusher = new Pusher($pusherKey, $pusherSecret, $pusherAppId);
 
         parent::__construct($level, $bubble);
     }

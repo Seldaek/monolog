54d89ef673ba7b0236696ad0721ef4d28e28dc49
Marius Krämer <marius.kraemer@mercoline.de>
2013-01-15 17:22:02 +0100

    Added test for ZmqHandler

---
+++ tests/Monolog/Handler/ZmqHandlerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Handler;
+
+use Monolog\TestCase;
+use Monolog\Logger;
+
+class ZmqHandlerTest extends TestCase
+{
+    public function setUp()
+    {
+        if (!class_exists('ZMQContext')) {
+            $this->markTestSkipped("php-zmq installed");
+        }
+    }
+
+    public function testHandle()
+    {
+        $record = $this->getRecord(Logger::WARNING, 'test', array('data' => new \stdClass, 'foo' => 34));
+
+        $socket = $this->getMockBuilder('ZMQSocket')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $socket->expects($this->once())
+            ->method('send')
+            ->with(json_encode($record));
+
+        $handler = new ZmqHandler($socket, 'log');
+        $handler->handle($record);
+    }
+
+}

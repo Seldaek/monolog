340c85b8cc2954e8190e72e33493baf46bc60856
Simon Mönch <sm@webfactory.de>
2013-02-26 10:48:50 +0100

    Bugfix

--- src/Monolog/Processor/UidProcessor.php
+++ src/Monolog/Processor/UidProcessor.php
@@ -18,18 +18,18 @@ namespace Monolog\Processor;
  */
 class UidProcessor 
 {
-    private static $uid;
+    private $uid;
     
     public function __construct() 
     {
-        if (null === self::$uid) {
-            self::$uid = substr(hash('md5', uniqid('', true)), 0, 7);
+        if (null === $this->uid) {
+            $this->uid = substr(hash('md5', uniqid('', true)), 0, 7);
         }
     }
 
     public function __invoke(array $record) 
     {
-        $record['extra']['uid'] = self::$uid;
+        $record['extra']['uid'] = $this->uid;
 
         return $record;
     }

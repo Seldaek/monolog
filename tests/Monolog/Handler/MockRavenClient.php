94e31e37b8c3dc702ee5b0ce197f43223bc6c9ca
David Windell <david@outeredgeuk.com>
2013-05-28 11:50:42 +0200

    API match

--- tests/Monolog/Handler/MockRavenClient.php
+++ tests/Monolog/Handler/MockRavenClient.php
@@ -15,7 +15,7 @@ use Raven_Client;
 
 class MockRavenClient extends Raven_Client
 {
-    public function capture($data, $stack)
+    public function capture($data, $stack, $vars = null)
     {
         $this->lastData = $data;
         $this->lastStack = $stack;

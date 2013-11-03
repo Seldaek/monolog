6c4c6ec15a774b92cd83a66ccb117bf69f98809b
Andrey Ryaguzov <dev.aeryaguzov@gmail.com>
2013-11-03 21:51:34 +0400

    pid processor

---
+++ tests/Monolog/Processor/PidProcessorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Processor;
+
+use Monolog\TestCase;
+
+class PidProcessorTest extends TestCase
+{
+    /**
+     * @covers Monolog\Processor\PidProcessor::__invoke
+     */
+    public function testProcessor()
+    {
+        $processor = new PidProcessor();
+        $record = $processor($this->getRecord());
+        $this->assertArrayHasKey('pid', $record['extra']);
+        $this->assertEquals(posix_getpid(), $record['extra']['pid']);
+    }
+}

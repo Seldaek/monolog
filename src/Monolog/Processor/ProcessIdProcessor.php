d5c30addcdd9795a2f4ed9155927ad9fcbf0e734
Andrey Ryaguzov <dev.aeryaguzov@gmail.com>
2013-11-04 12:15:17 +0400

    add right pid for childs

--- src/Monolog/Processor/ProcessIdProcessor.php
+++ src/Monolog/Processor/ProcessIdProcessor.php
@@ -18,22 +18,13 @@ namespace Monolog\Processor;
  */
 class ProcessIdProcessor
 {
-    private static $pid;
-
-    public function __construct()
-    {
-        if (null === self::$pid) {
-            self::$pid = getmypid();
-        }
-    }
-
     /**
      * @param  array $record
      * @return array
      */
     public function __invoke(array $record)
     {
-        $record['extra']['process_id'] = self::$pid;
+        $record['extra']['process_id'] = getmypid();
 
         return $record;
     }

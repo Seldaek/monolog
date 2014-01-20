2ba2cd910a94b9b527edcb0eb46987bc3b4e29d9
Adam Pancutt <adam@panadas.co.uk>
2014-01-20 18:48:01 +0000

    Added support for Loggly batch log uploads

---
+++ src/Monolog/Formatter/LogglyFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Formatter;
+
+/**
+ * Extends the standard JsonFormatter to format messages compatible with Loggly.
+ *
+ * @author Adam Pancutt <adam@pancutt.com>
+ */
+class LogglyFormatter extends JsonFormatter
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function formatBatch(array $records)
+    {
+        array_walk($records, function(&$value, $key) {
+            $value = $this->format($value);
+        });
+
+        return implode("\n", $records);
+    }
+}

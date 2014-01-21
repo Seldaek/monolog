70290e7087e1954f7c885682f5a6e9ea262a6739
Adam Pancutt <adam@panadas.co.uk>
2014-01-21 11:47:47 +0000

    Added batch mode tests to JsonFormatter

--- tests/Monolog/Formatter/JsonFormatterTest.php
+++ tests/Monolog/Formatter/JsonFormatterTest.php
@@ -17,6 +17,18 @@ use Monolog\TestCase;
 class JsonFormatterTest extends TestCase
 {
     /**
+     * @covers Monolog\Formatter\JsonFormatter::__construct
+     * @covers Monolog\Formatter\JsonFormatter::getBatchMode
+     */
+    public function testConstruct()
+    {
+        $formatter = new JsonFormatter();
+        $this->assertEquals(JsonFormatter::BATCH_MODE_JSON, $formatter->getBatchMode());
+        $formatter = new JsonFormatter(JsonFormatter::BATCH_MODE_NEWLINES);
+        $this->assertEquals(JsonFormatter::BATCH_MODE_NEWLINES, $formatter->getBatchMode());
+    }
+
+    /**
      * @covers Monolog\Formatter\JsonFormatter::format
      */
     public function testFormat()
@@ -28,6 +40,7 @@ class JsonFormatterTest extends TestCase
 
     /**
      * @covers Monolog\Formatter\JsonFormatter::formatBatch
+     * @covers Monolog\Formatter\JsonFormatter::formatBatchJson
      */
     public function testFormatBatch()
     {
@@ -37,5 +50,23 @@ class JsonFormatterTest extends TestCase
             $this->getRecord(Logger::DEBUG),
         );
         $this->assertEquals(json_encode($records), $formatter->formatBatch($records));
+    }
+
+    /**
+     * @covers Monolog\Formatter\JsonFormatter::formatBatch
+     * @covers Monolog\Formatter\JsonFormatter::formatBatchNewlines
+     */
+    public function testFormatBatchNewlines()
+    {
+
+        $formatter = new JsonFormatter(JsonFormatter::BATCH_MODE_NEWLINES);
+        $records = $expected = array(
+            $this->getRecord(Logger::WARNING),
+            $this->getRecord(Logger::DEBUG),
+        );
+        array_walk($expected, function(&$value, $key) {
+            $value = json_encode($value);
+        });
+        $this->assertEquals(implode("\n", $expected), $formatter->formatBatch($records));
     }
 }

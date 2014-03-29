2b6dffa23ffe27812564f39164b8aa246b57f7bf
Robert Kaufmann III <rok3@rokthree.com>
2014-03-28 22:55:15 -0400

    Add handler and tests for logentries.com

---
+++ src/Monolog/Handler/LogEntriesHandler.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Handler;
+
+use Monolog\Logger;
+use Monolog\Formatter\LineFormatter;
+
+/**
+ * @author Robert Kaufmann III <rok3@rok3.me>
+ */
+class LogEntriesHandler extends SocketHandler
+{
+
+    /**
+     * @var string
+     */
+    protected $logToken;
+
+    /**
+     * @param string  $token  Log token supplied by LogEntries
+     * @param int     $level  The minimum logging level to trigger this handler
+     * @param boolean $bubble Whether or not messages that are handled should bubble up the stack.
+     * @param boolean $useSSL Whether or not SSL encryption should be used.
+     * @param string  $format Message format. See Monolog usage documents.
+     *
+     * @throws MissingExtensionExcpetion If SSL encryption is set to true and OpenSSL is missing
+     */
+    public function __construct($token, $level = Logger::DEBUG, $bubble = true, $useSSL = true)
+    {
+        if ($useSSL && !extension_loaded('openssl')) {
+            throw new MissingExtensionException('The OpenSSL PHP plugin is required to use SSL encrypted connection for LogEntriesHandler');
+        }
+
+        $endpoint = $useSSL ? 'ssl://api.logentries.com:20000' : 'data.logentries.com:80';
+        parent::__construct($endpoint, $level, $bubble);
+        $this->logToken = $token;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array $record
+     */
+    public function write(array $record)
+    {
+        parent::write($record);
+        $this->closeSocket();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleBatch(array $records)
+    {
+        foreach ($records as $record) {
+            $record['formatted'] = $this->getFormatter()->format($record);
+            parent::write($record);
+        }
+        $this->closeSocket();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param  array  $record
+     * @return string
+     */
+    protected function generateDataStream($record)
+    {
+        return $this->logToken . ' ' . $record['formatted'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getDefaultFormatter()
+    {
+        return new LineFormatter();
+    }
+}

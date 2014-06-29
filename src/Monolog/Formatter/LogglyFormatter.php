7d3fb791a3e5459e2157927bb75e03981568a2d0
Jordi Boggiano <j.boggiano@seld.be>
2014-06-29 12:16:42 +0200

    Fix loggly formatter on php 5.6

--- src/Monolog/Formatter/LogglyFormatter.php
+++ src/Monolog/Formatter/LogglyFormatter.php
@@ -38,7 +38,7 @@ class LogglyFormatter extends JsonFormatter
     public function format(array $record)
     {
         if (isset($record["datetime"]) && ($record["datetime"] instanceof \DateTime)) {
-            $record["timestamp"] = $record["datetime"]->format("c");
+            $record["timestamp"] = $record["datetime"]->format("Y-m-d\TH:i:s.uO");
             // TODO 2.0 unset the 'datetime' parameter, retained for BC
         }
 

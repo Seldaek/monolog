ce7a8b63e11106cba19d6b69b6992ac5b11c5dac
Menno Holtkamp <menno.holtkamp@weev.nl>
2014-07-31 13:35:17 +0200

    Removed need to override createFromFormat()

--- src/Monolog/DateTime.php
+++ src/Monolog/DateTime.php
@@ -20,23 +20,6 @@ class DateTime extends \DateTime implements \JsonSerializable
 {
 
     /**
-     * Override function to ensure an object of this class is returned
-     *
-     * @link http://stackoverflow.com/questions/17909871/getting-date-format-m-d-y-his-u-from-milliseconds
-     * @param string $format
-     * @param string $time
-     * @param DateTimeZone $timezone
-     * @return static
-     */
-    public static function createFromFormat($format, $time, $timezone = null)
-    {
-        $timestamp = microtime(true);
-        $microseconds = sprintf('%06d', ($timestamp - floor($timestamp)) * 1000000);
-
-        return new static(date('Y-m-d H:i:s.' . $microseconds, $timestamp));
-    }
-
-    /**
      * Append the microseconds to the date property
      *
      * @return array
@@ -47,7 +30,6 @@ class DateTime extends \DateTime implements \JsonSerializable
         $result['date'] .= $this->format('.u');
 
         return $result;
-
     }
 
 }

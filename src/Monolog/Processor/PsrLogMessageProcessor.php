b3f039339d7a8173c7b441dbaa572ccacb712b54
Jordi Boggiano <j.boggiano@seld.be>
2014-09-10 16:41:01 +0100

    Support objects/arrays/resources a bit better, refs #410

--- src/Monolog/Processor/PsrLogMessageProcessor.php
+++ src/Monolog/Processor/PsrLogMessageProcessor.php
@@ -32,10 +32,12 @@ class PsrLogMessageProcessor
 
         $replacements = array();
         foreach ($record['context'] as $key => $val) {
-            if (is_null($val) || 
-                is_scalar($val) || 
-               (is_object($val) && method_exists($val, "__toString"))) {
+            if (is_null($val) || is_scalar($val) || (is_object($val) && method_exists($val, "__toString"))) {
                 $replacements['{'.$key.'}'] = $val;
+            } elseif (is_object($val)) {
+                $replacements['{'.$key.'}'] = '[object '.get_class($val).']';
+            } else {
+                $replacements['{'.$key.'}'] = '['.gettype($val).']';
             }
         }
 

05936033f7c03532ccaff7ec89f3faa52e7d5742
Adam Nicholson <adamnicholson10@gmail.com>
2014-09-29 20:51:07 +0100

    Removed Guzzle dependency from Mandrill handler. API calls are now done via cURL

--- src/Monolog/Handler/MandrillHandler.php
+++ src/Monolog/Handler/MandrillHandler.php
@@ -12,10 +12,9 @@
 namespace Monolog\Handler;
 
 use Monolog\Logger;
-use GuzzleHttp\Client;
 
 /**
- * MandrillHandler uses GuzzleHttp to send the emails to the Mandrill API
+ * MandrillHandler uses cURL to send the emails to the Mandrill API
  *
  * @author Adam Nicholson <adamnicholson10@gmail.com>
  */
@@ -25,16 +24,15 @@ class MandrillHandler extends MailHandler
     protected $message;
 
     /**
-     * @param \GuzzleHttp\Client      $client  The Guzzle client
      * @oaram string                  $apiKey  A valid Mandrill API key
      * @param callable|\Swift_Message $message An example message for real messages, only the body will be replaced
      * @param integer                 $level   The minimum logging level at which this handler will be triggered
      * @param Boolean                 $bubble  Whether the messages that are handled can bubble up the stack or not
      */
-    public function __construct(Client $client, $apiKey, $message, $level = Logger::ERROR, $bubble = true)
+    public function __construct($apiKey, $message, $level = Logger::ERROR, $bubble = true)
     {
         parent::__construct($level, $bubble);
-        $this->client  = $client;
+
         if (!$message instanceof \Swift_Message && is_callable($message)) {
             $message = call_user_func($message);
         }
@@ -53,12 +51,17 @@ class MandrillHandler extends MailHandler
         $message = clone $this->message;
         $message->setBody($content);
 
-        $this->client->post('https://mandrillapp.com/api/1.0/messages/send-raw.json', [
-            'body' => [
-                'key' => $this->apiKey,
-                'raw_message' => (string) $message,
-                'async' => false,
-            ],
-        ]);
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://mandrillapp.com/api/1.0/messages/send-raw.json');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+            'key' => $this->apiKey,
+            'raw_message' => (string) $message,
+            'async' => false,
+        )));
+
+        curl_exec($ch);
+        curl_close ($ch);
     }
 }

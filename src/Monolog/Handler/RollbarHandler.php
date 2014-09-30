31358c858fa07cf0c6ec5c6a381c10c5ff0154f1
Jordi Boggiano <j.boggiano@seld.be>
2014-09-30 14:24:28 +0100

    Add missing license headers

--- src/Monolog/Handler/RollbarHandler.php
+++ src/Monolog/Handler/RollbarHandler.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Monolog\Handler;
 
 use RollbarNotifier;

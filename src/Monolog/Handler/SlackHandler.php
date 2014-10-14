20b56007adc2e0204b87409ee519652364f2f58c
Patrick Landolt <patrick.landolt@artack.ch>
2014-10-14 10:53:50 +0200

    added the option to include extra data and to chose for a smaller output but with the same information available

--- src/Monolog/Handler/SlackHandler.php
+++ src/Monolog/Handler/SlackHandler.php
@@ -52,6 +52,18 @@ class SlackHandler extends SocketHandler
     private $useAttachment;
 
     /**
+     * Whether the the message that is added to Slack as attachment is in a short style (or not)
+     * @var bool
+     */
+    private $useShortAttachment;
+
+    /**
+     * Whether the attachment should include extra data (or not)
+     * @var bool
+     */
+    private $includeExtra;
+
+    /**
      * @param string      $token         Slack API token
      * @param string      $channel       Slack channel (encoded ID or name)
      * @param string      $username      Name of a bot
@@ -60,7 +72,7 @@ class SlackHandler extends SocketHandler
      * @param int         $level         The minimum logging level at which this handler will be triggered
      * @param bool        $bubble        Whether the messages that are handled can bubble up the stack or not
      */
-    public function __construct($token, $channel, $username = 'Monolog', $useAttachment = true, $iconEmoji = null, $level = Logger::CRITICAL, $bubble = true)
+    public function __construct($token, $channel, $username = 'Monolog', $useAttachment = true, $iconEmoji = null, $level = Logger::CRITICAL, $bubble = true, $useShortAttachment = false, $includeExtra = false)
     {
         if (!extension_loaded('openssl')) {
             throw new MissingExtensionException('The OpenSSL PHP extension is required to use the SlackHandler');
@@ -73,6 +85,8 @@ class SlackHandler extends SocketHandler
         $this->username = $username;
         $this->iconEmoji = trim($iconEmoji, ':');
         $this->useAttachment = $useAttachment;
+        $this->useShortAttachment = $useShortAttachment;
+        $this->includeExtra = $includeExtra;
     }
 
     /**
@@ -104,27 +118,52 @@ class SlackHandler extends SocketHandler
             'attachments' => array()
         );
 
+        $extra = '';
+        foreach ($record['extra'] as $var => $val) {
+            $extra .= $var.': '.$this->replaceNewlines($this->convertToString($val))." | ";
+        }
+
+        $extra = rtrim($extra, " |");
+
         if ($this->useAttachment) {
-            $dataArray['attachments'] = json_encode(
-                array(
-                    array(
-                        'fallback' => $record['message'],
-                        'color' => $this->getAttachmentColor($record['level']),
-                        'fields' => array(
-                            array(
-                                'title' => 'Message',
-                                'value' => $record['message'],
-                                'short' => false
-                            ),
-                            array(
-                                'title' => 'Level',
-                                'value' => $record['level_name'],
-                                'short' => true
-                            )
-                        )
-                    )
-                )
+
+            $attachment = array(
+                'fallback' => $record['message'],
+                'color' => $this->getAttachmentColor($record['level'])
             );
+
+            if ($this->useShortAttachment) {
+                $attachment['fields'] = array(
+                    array(
+                        'title' => $record['level_name'],
+                        'value' => $record['message'],
+                        'short' => false
+                    )
+                );
+            } else {
+                $attachment['fields'] = array(
+                    array(
+                        'title' => 'Message',
+                        'value' => $record['message'],
+                        'short' => false
+                    ),
+                    array(
+                        'title' => 'Level',
+                        'value' => $record['level_name'],
+                        'short' => true
+                    )
+                );
+            }
+
+            if ($this->includeExtra) {
+                $attachment['fields'][] = array(
+                    'title' => "Extra",
+                    'value' => $extra,
+                    'short' => false
+                );
+            }
+
+            $dataArray['attachments'] = json_encode(array($attachment));
         } else {
             $dataArray['text'] = $record['message'];
         }
@@ -183,5 +222,55 @@ class SlackHandler extends SocketHandler
             default:
                 return '#e3e4e6';
         }
+    }
+
+    /**
+     * Copy from LineFormater (any better idea?)
+     */
+    protected function convertToString($data)
+    {
+        if (null === $data || is_bool($data)) {
+            return var_export($data, true);
+        }
+
+        if (is_scalar($data)) {
+            return (string) $data;
+        }
+
+        if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
+            return $this->toJson($data, true);
+        }
+
+        return str_replace('\\/', '/', @json_encode($data));
+    }
+
+
+    /**
+     * Copy from LineFormater (any better idea?)
+     */
+    protected function toJson($data, $ignoreErrors = false)
+    {
+        // suppress json_encode errors since it's twitchy with some inputs
+        if ($ignoreErrors) {
+            if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
+                return @json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+            }
+
+            return @json_encode($data);
+        }
+
+        if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
+            return json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+
+        return json_encode($data);
+    }
+
+    /**
+     * Copy from LineFormater (any better idea?)
+     */
+    protected function replaceNewlines($str)
+    {
+        return strtr($str, array("\r\n" => ' ', "\r" => ' ', "\n" => ' '));
     }
 }

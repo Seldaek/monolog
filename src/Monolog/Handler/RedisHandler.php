5037d264b1b5e0b0dc36179ffd24c9a10b3bae62
Carson Full <carson.full@gmomail.org>
2015-04-24 16:34:56 -0500

    Updated RedisHandler constructor's docblock

--- src/Monolog/Handler/RedisHandler.php
+++ src/Monolog/Handler/RedisHandler.php
@@ -11,8 +11,8 @@
 
 namespace Monolog\Handler;
 
-use Monolog\Logger;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Logger;
 
 /**
  * Logs to a Redis key using rpush
@@ -30,7 +30,12 @@ class RedisHandler extends AbstractProcessingHandler
     private $redisClient;
     private $redisKey;
 
-    # redis instance, key to use
+    /**
+     * @param \Predis\Client|\Redis $redis  The redis instance
+     * @param string                $key    The key name to push records to
+     * @param integer               $level  The minimum logging level at which this handler will be triggered
+     * @param boolean               $bubble Whether the messages that are handled can bubble up the stack or not
+     */
     public function __construct($redis, $key, $level = Logger::DEBUG, $bubble = true)
     {
         if (!(($redis instanceof \Predis\Client) || ($redis instanceof \Redis))) {

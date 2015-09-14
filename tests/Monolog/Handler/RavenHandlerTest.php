5405308eb1c7dc1fdc0bd408f80eca2ee8ed9a92
Jordi Boggiano <j.boggiano@seld.be>
2015-09-14 17:45:49 +0100

    Fix raven tests

--- tests/Monolog/Handler/RavenHandlerTest.php
+++ tests/Monolog/Handler/RavenHandlerTest.php
@@ -117,7 +117,7 @@ class RavenHandlerTest extends TestCase
         $ravenClient->user_context(array('id' => 'test_user_id'));
         // handle context
         $handler->handle($recordWithContext);
-        $this->assertEquals($user, $ravenClient->lastData['sentry.interfaces.User']);
+        $this->assertEquals($user, $ravenClient->lastData['user']);
 
         // check to see if its reset
         $handler->handle($recordWithNoContext);
@@ -127,7 +127,7 @@ class RavenHandlerTest extends TestCase
         // handle with null context
         $ravenClient->user_context(null);
         $handler->handle($recordWithContext);
-        $this->assertEquals($user, $ravenClient->lastData['sentry.interfaces.User']);
+        $this->assertEquals($user, $ravenClient->lastData['user']);
 
         // check to see if its reset
         $handler->handle($recordWithNoContext);

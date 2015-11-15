a5552dd34d9493cc18ba443d773de709842ebf8d
fordnox <fordnox@gmail.com>
2015-11-15 20:39:30 +0200

    Add fluentd formatter.

---
+++ tests/Monolog/Formatter/FluentdFormatterTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Formatter;
+
+use Monolog\Logger;
+use Monolog\TestCase;
+
+class FluentdFormatterTest extends TestCase
+{
+    /**
+     * @covers Monolog\Formatter\FluentdFormatter::__construct
+     * @covers Monolog\Formatter\FluentdFormatter::isUsingLevelsInTag
+     */
+    public function testConstruct()
+    {
+        $formatter = new FluentdFormatter();
+        $this->assertEquals(false, $formatter->isUsingLevelsInTag());
+        $formatter = new FluentdFormatter(false);
+        $this->assertEquals(false, $formatter->isUsingLevelsInTag());
+        $formatter = new FluentdFormatter(true);
+        $this->assertEquals(true, $formatter->isUsingLevelsInTag());
+    }
+
+    /**
+     * @covers Monolog\Formatter\FluentdFormatter::format
+     */
+    public function testFormat()
+    {
+
+        $record = $this->getRecord(Logger::WARNING);
+        $record['datetime'] = new \DateTime("@0");
+
+        $formatter = new FluentdFormatter();
+        $this->assertEquals(
+            '["test", 0, {"message":"test","level":300,"level_name":"WARNING","extra":[]}]',
+            $formatter->format($record));
+
+        $formatter = new FluentdFormatter(true);
+        $this->assertEquals(
+            '["test.warning", 0, {"message":"test","level":300,"level_name":"WARNING","extra":[]}]',
+            $formatter->format($record));
+    }
+}

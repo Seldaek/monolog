f98325fd0cd70caf8fd5980ea45e69b018d678a1
Andrej Hudec <pulzarraider2@gmail.com>
2016-01-05 09:11:24 +0100

    Changed LOG_LOCALx constants to their numeric values.
    LOG_LOCALx constants are unavailable on Windows so we cannot used them in cross-platform code, but the local facilities has to be defined to avoid throwing "Unknown facility value..." exceptions on Windows.

--- src/Monolog/Handler/AbstractSyslogHandler.php
+++ src/Monolog/Handler/AbstractSyslogHandler.php
@@ -61,16 +61,14 @@ abstract class AbstractSyslogHandler extends AbstractProcessingHandler
     {
         parent::__construct($level, $bubble);
 
-        if (!defined('PHP_WINDOWS_VERSION_BUILD')) {
-            $this->facilities['local0'] = LOG_LOCAL0;
-            $this->facilities['local1'] = LOG_LOCAL1;
-            $this->facilities['local2'] = LOG_LOCAL2;
-            $this->facilities['local3'] = LOG_LOCAL3;
-            $this->facilities['local4'] = LOG_LOCAL4;
-            $this->facilities['local5'] = LOG_LOCAL5;
-            $this->facilities['local6'] = LOG_LOCAL6;
-            $this->facilities['local7'] = LOG_LOCAL7;
-        }
+        $this->facilities['local0'] = 128; // LOG_LOCAL0
+        $this->facilities['local1'] = 136; // LOG_LOCAL1
+        $this->facilities['local2'] = 144; // LOG_LOCAL2
+        $this->facilities['local3'] = 152; // LOG_LOCAL3
+        $this->facilities['local4'] = 160; // LOG_LOCAL4
+        $this->facilities['local5'] = 168; // LOG_LOCAL5
+        $this->facilities['local6'] = 176; // LOG_LOCAL6
+        $this->facilities['local7'] = 184; // LOG_LOCAL7
 
         // convert textual description of facility to syslog constant
         if (array_key_exists(strtolower($facility), $this->facilities)) {

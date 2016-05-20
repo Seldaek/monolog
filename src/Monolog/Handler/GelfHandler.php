e46fecde340c873dc6100422b00cff732963947a
Jordi Boggiano <j.boggiano@seld.be>
2016-05-20 19:55:04 +0100

    Remove support for legacy Gelf lib

--- src/Monolog/Handler/GelfHandler.php
+++ src/Monolog/Handler/GelfHandler.php
@@ -11,9 +11,7 @@
 
 namespace Monolog\Handler;
 
-use Gelf\IMessagePublisher;
 use Gelf\PublisherInterface;
-use Gelf\Publisher;
 use InvalidArgumentException;
 use Monolog\Logger;
 use Monolog\Formatter\GelfMessageFormatter;
@@ -33,17 +31,13 @@ class GelfHandler extends AbstractProcessingHandler
     protected $publisher;
 
     /**
-     * @param PublisherInterface|IMessagePublisher|Publisher $publisher a publisher object
-     * @param int                                            $level     The minimum logging level at which this handler will be triggered
-     * @param bool                                           $bubble    Whether the messages that are handled can bubble up the stack or not
+     * @param PublisherInterface $publisher a publisher object
+     * @param int                $level     The minimum logging level at which this handler will be triggered
+     * @param bool               $bubble    Whether the messages that are handled can bubble up the stack or not
      */
-    public function __construct($publisher, $level = Logger::DEBUG, $bubble = true)
+    public function __construct(PublisherInterface $publisher, $level = Logger::DEBUG, $bubble = true)
     {
         parent::__construct($level, $bubble);
-
-        if (!$publisher instanceof Publisher && !$publisher instanceof IMessagePublisher && !$publisher instanceof PublisherInterface) {
-            throw new InvalidArgumentException('Invalid publisher, expected a Gelf\Publisher, Gelf\IMessagePublisher or Gelf\PublisherInterface instance');
-        }
 
         $this->publisher = $publisher;
     }

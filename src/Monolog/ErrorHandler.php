d9be260f3d8adf94b141d72a8bcac905bf79a4c9
s4msung <s4msung@exigen.org>
2016-06-27 18:22:06 +0200

    include $context in error_handler

--- src/Monolog/ErrorHandler.php
+++ src/Monolog/ErrorHandler.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -149,7 +150,7 @@ class ErrorHandler
         // fatal error codes are ignored if a fatal error handler is present as well to avoid duplicate log entries
         if (!$this->hasFatalErrorHandler || !in_array($code, self::$fatalErrors, true)) {
             $level = isset($this->errorLevelMap[$code]) ? $this->errorLevelMap[$code] : LogLevel::CRITICAL;
-            $this->logger->log($level, self::codeToString($code).': '.$message, array('code' => $code, 'message' => $message, 'file' => $file, 'line' => $line));
+            $this->logger->log($level, self::codeToString($code).': '.$message, array('code' => $code, 'message' => $message, 'file' => $file, 'line' => $line, 'context' => $context));
         }
 
         if ($this->previousErrorHandler === true) {

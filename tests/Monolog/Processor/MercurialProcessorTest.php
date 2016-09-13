240b7458dd312c26ed335d9af14b8ed7a809f5ae
Jonathan A. Schweder <jonathanschweder@gmail.com>
2016-09-13 12:27:52 -0300

    add MercurialProcessor

---
+++ tests/Monolog/Processor/MercurialProcessorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jonathan A. Schweder <jonathanschweder@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Processor;
+
+use Monolog\TestCase;
+
+class MercurialProcessorTest extends TestCase
+{
+    /**
+     * @covers Monolog\Processor\MercurialProcessor::__invoke
+     */
+    public function testProcessor()
+    {
+        `hg init`;
+        $processor = new MercurialProcessor();
+        $record = $processor($this->getRecord());
+
+        $this->assertArrayHasKey('hg', $record['extra']);
+        $this->assertTrue(!is_array($record['extra']['hg']['branch']));
+        $this->assertTrue(!is_array($record['extra']['hg']['revision']));
+    }
+}

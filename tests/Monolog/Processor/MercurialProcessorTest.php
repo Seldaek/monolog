ffc421a4fd4e9651e236341f5ff3994b25a8ecc5
Jordi Boggiano <j.boggiano@seld.be>
2016-09-25 16:47:31 +0200

    Make hg test optional

--- tests/Monolog/Processor/MercurialProcessorTest.php
+++ tests/Monolog/Processor/MercurialProcessorTest.php
@@ -20,6 +20,16 @@ class MercurialProcessorTest extends TestCase
      */
     public function testProcessor()
     {
+        if (defined('PHP_WINDOWS_VERSION_BUILD')) {
+            exec("where hg 2>NUL", $output, $result);
+        } else {
+            exec("which hg 2>/dev/null >/dev/null", $output, $result);
+        }
+        if ($result != 0) {
+            $this->markTestSkipped('hg is missing');
+            return;
+        }
+
         `hg init`;
         $processor = new MercurialProcessor();
         $record = $processor($this->getRecord());

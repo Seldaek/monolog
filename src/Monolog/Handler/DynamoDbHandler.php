d72b886978e6878fb55d163ccbbc1a4cf6427f47
Jordi Boggiano <j.boggiano@seld.be>
2016-09-29 09:17:17 +0200

    Add support for AWS SDK v3

--- src/Monolog/Handler/DynamoDbHandler.php
+++ src/Monolog/Handler/DynamoDbHandler.php
@@ -13,6 +13,7 @@ namespace Monolog\Handler;
 
 use Aws\Common\Aws;
 use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Marshaler;
 use Monolog\Formatter\ScalarFormatter;
 use Monolog\Logger;
 
@@ -37,6 +38,16 @@ class DynamoDbHandler extends AbstractProcessingHandler
     protected $table;
 
     /**
+     * @var int
+     */
+    protected $version;
+
+    /**
+     * @var Marshaler
+     */
+    protected $marshaler;
+
+    /**
      * @param DynamoDbClient $client
      * @param string         $table
      * @param int            $level
@@ -44,8 +55,11 @@ class DynamoDbHandler extends AbstractProcessingHandler
      */
     public function __construct(DynamoDbClient $client, $table, $level = Logger::DEBUG, $bubble = true)
     {
-        if (!defined('Aws\Common\Aws::VERSION') || version_compare('3.0', Aws::VERSION, '<=')) {
-            throw new \RuntimeException('The DynamoDbHandler is only known to work with the AWS SDK 2.x releases');
+        if (defined('Aws\Common\Aws::VERSION') && version_compare(Aws::VERSION, '3.0', '>=')) {
+            $this->version = 3;
+            $this->marshaler = new Marshaler;
+        } else {
+            $this->version = 2;
         }
 
         $this->client = $client;
@@ -60,7 +74,11 @@ class DynamoDbHandler extends AbstractProcessingHandler
     protected function write(array $record)
     {
         $filtered = $this->filterEmptyFields($record['formatted']);
-        $formatted = $this->client->formatAttributes($filtered);
+        if ($this->version === 3) {
+            $formatted = $this->marshaler->marshalItem($filtered);
+        } else {
+            $formatted = $this->client->formatAttributes($filtered);
+        }
 
         $this->client->putItem(array(
             'TableName' => $this->table,

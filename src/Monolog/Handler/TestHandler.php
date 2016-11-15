600aa6e653876262d12fcdcb5c28654915ebbb06
Julien Falque <julien.falque@gmail.com>
2016-11-15 22:35:13 +0100

    Make protected generic methods public

--- src/Monolog/Handler/TestHandler.php
+++ src/Monolog/Handler/TestHandler.php
@@ -79,12 +79,12 @@ class TestHandler extends AbstractProcessingHandler
         $this->recordsByLevel = array();
     }
 
-    protected function hasRecordRecords($level)
+    public function hasRecords($level)
     {
         return isset($this->recordsByLevel[$level]);
     }
 
-    protected function hasRecord($record, $level)
+    public function hasRecord($record, $level)
     {
         if (is_array($record)) {
             $record = $record['message'];
@@ -140,7 +140,7 @@ class TestHandler extends AbstractProcessingHandler
     public function __call($method, $args)
     {
         if (preg_match('/(.*)(Debug|Info|Notice|Warning|Error|Critical|Alert|Emergency)(.*)/', $method, $matches) > 0) {
-            $genericMethod = $matches[1] . 'Record' . $matches[3];
+            $genericMethod = $matches[1] . ('Records' !== $matches[3] ? 'Record' : '') . $matches[3];
             $level = constant('Monolog\Logger::' . strtoupper($matches[2]));
             if (method_exists($this, $genericMethod)) {
                 $args[] = $level;

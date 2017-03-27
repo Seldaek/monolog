4260b467605950f2de533b43dab15b92324acd2b
Harm Bandstra <hbandstra@gmail.com>
2017-03-27 16:57:59 +0200

    SlackWebhookHandler: refactor curl_setopt to curl_setopt_array

--- src/Monolog/Handler/SlackWebhookHandler.php
+++ src/Monolog/Handler/SlackWebhookHandler.php
@@ -81,14 +81,18 @@ class SlackWebhookHandler extends AbstractProcessingHandler
         $postString = json_encode($postData);
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->webhookUrl);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $options = [
+            CURLOPT_URL => $this->webhookUrl,
+            CURLOPT_POST => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => ['Content-type: application/json'],
+            CURLOPT_POSTFIELDS => $postString
+        ];
         if (defined('CURLOPT_SAFE_UPLOAD')) {
-            curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+            $options[CURLOPT_SAFE_UPLOAD] = true;
         }
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+
+        curl_setopt_array($ch, $options);
 
         Curl\Util::execute($ch);
     }

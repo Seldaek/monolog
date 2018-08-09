bff2c8488e69dc7f2a6fa8f50ec02a5f2c07009e
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-08-09 08:21:24 +0200

    Update Raven client package URL

--- src/Monolog/Handler/RavenHandler.php
+++ src/Monolog/Handler/RavenHandler.php
@@ -18,7 +18,7 @@ use Raven_Client;
 
 /**
  * Handler to send messages to a Sentry (https://github.com/getsentry/sentry) server
- * using raven-php (https://github.com/getsentry/raven-php)
+ * using sentry-php (https://github.com/getsentry/sentry-php)
  *
  * @author Marc Abramowitz <marc@marc-abramowitz.com>
  */

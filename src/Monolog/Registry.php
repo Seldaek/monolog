ceebd568a2ffa1003c6a84fb9c53162bb96fd646
Jordi Boggiano <j.boggiano@seld.be>
2018-11-04 18:41:02 +0100

    Remove getClass func from Registry as it was moved to Utils class

--- src/Monolog/Registry.php
+++ src/Monolog/Registry.php
@@ -131,14 +131,4 @@ class Registry
     {
         return self::getInstance($name);
     }
-
-    /**
-     * @internal
-     */
-    public function getClass($object)
-    {
-        $class = \get_class($object);
-
-        return 'c' === $class[0] && 0 === strpos($class, "class@anonymous\0") ? get_parent_class($class).'@anonymous' : $class;
-    }
 }

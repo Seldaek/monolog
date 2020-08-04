fa78dd44004221145701cf3fcdf66d61ebbbce7e
Dino Korah <dino.korah@redmatter.com>
2020-08-04 12:09:20 +0100

    Backport PsrLogMessageProcessor from master (2.x)

--- src/Monolog/Processor/PsrLogMessageProcessor.php
+++ src/Monolog/Processor/PsrLogMessageProcessor.php
@@ -22,6 +22,24 @@ use Monolog\Utils;
  */
 class PsrLogMessageProcessor implements ProcessorInterface
 {
+    const SIMPLE_DATE = "Y-m-d\TH:i:s.uP";
+
+    /** @var string|null */
+    private $dateFormat;
+
+    /** @var bool */
+    private $removeUsedContextFields;
+
+    /**
+     * @param string|null $dateFormat              The format of the timestamp: one supported by DateTime::format
+     * @param bool        $removeUsedContextFields If set to true the fields interpolated into message gets unset
+     */
+    public function __construct($dateFormat = null, $removeUsedContextFields = false)
+    {
+        $this->dateFormat = $dateFormat;
+        $this->removeUsedContextFields = $removeUsedContextFields;
+    }
+
     /**
      * @param  array $record
      * @return array
@@ -34,12 +52,25 @@ class PsrLogMessageProcessor implements ProcessorInterface
 
         $replacements = array();
         foreach ($record['context'] as $key => $val) {
+            $placeholder = '{' . $key . '}';
+            if (strpos($record['message'], $placeholder) === false) {
+                continue;
+            }
+
             if (is_null($val) || is_scalar($val) || (is_object($val) && method_exists($val, "__toString"))) {
-                $replacements['{'.$key.'}'] = $val;
+                $replacements[$placeholder] = $val;
+            } elseif ($val instanceof \DateTime) {
+                $replacements[$placeholder] = $val->format($this->dateFormat ?: static::SIMPLE_DATE);
             } elseif (is_object($val)) {
-                $replacements['{'.$key.'}'] = '[object '.Utils::getClass($val).']';
+                $replacements[$placeholder] = '[object '.Utils::getClass($val).']';
+            } elseif (is_array($val)) {
+                $replacements[$placeholder] = 'array'.@json_encode($val);
             } else {
-                $replacements['{'.$key.'}'] = '['.gettype($val).']';
+                $replacements[$placeholder] = '['.gettype($val).']';
+            }
+
+            if ($this->removeUsedContextFields) {
+                unset($record['context'][$key]);
             }
         }
 

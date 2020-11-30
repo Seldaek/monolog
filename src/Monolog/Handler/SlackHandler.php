6087181108f9c8f82184aa388b6813025b3e2cf4
Victor Bocharsky <bocharsky.bw@gmail.com>
2020-11-30 23:01:43 +0200

    Remove duplicated article in PHPDoc comment

--- src/Monolog/Handler/SlackHandler.php
+++ src/Monolog/Handler/SlackHandler.php
@@ -44,7 +44,7 @@ class SlackHandler extends SocketHandler
      * @param  string|null               $iconEmoji              The emoji name to use (or null)
      * @param  int                       $level                  The minimum logging level at which this handler will be triggered
      * @param  bool                      $bubble                 Whether the messages that are handled can bubble up the stack or not
-     * @param  bool                      $useShortAttachment     Whether the the context/extra messages added to Slack as attachments are in a short style
+     * @param  bool                      $useShortAttachment     Whether the context/extra messages added to Slack as attachments are in a short style
      * @param  bool                      $includeContextAndExtra Whether the attachment should include context and extra data
      * @param  array                     $excludeFields          Dot separated list of fields to exclude from slack message. E.g. ['context.field1', 'extra.field2']
      * @throws MissingExtensionException If no OpenSSL PHP extension configured

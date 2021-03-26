18148be5ded30a815e54d569666e9cf775680c78
Alexandr Mazur <amazur@xmlshop.com>
2021-03-26 13:26:26 +0200

    Added tests for handling too long message

--- tests/Monolog/Handler/TelegramBotHandlerTest.php
+++ tests/Monolog/Handler/TelegramBotHandlerTest.php
@@ -11,6 +11,7 @@
 
 namespace Monolog\Handler;
 
+use Generator;
 use Monolog\Logger;
 use Monolog\Test\TestCase;
 
@@ -20,9 +21,7 @@ use Monolog\Test\TestCase;
  */
 class TelegramBotHandlerTest extends TestCase
 {
-    /**
-     * @var TelegramBotHandler
-     */
+    /** @var TelegramBotHandler */
     private $handler;
 
     public function testSendTelegramRequest(): void
@@ -32,33 +31,32 @@ class TelegramBotHandlerTest extends TestCase
     }
 
     /**
-     * @param string $apiKey
-     * @param string $channel
+     * @dataProvider tooLongMessageHandlingProvider
+     * @param string $strategy
+     * @param int $messageLength
+     * @param int $expectedRequestsCounts
+     * @return void
      */
-    private function createHandler(
-        string $apiKey = 'testKey',
-        string $channel = 'testChannel',
-        string $parseMode = 'Markdown',
-        bool $disableWebPagePreview = false,
-        bool $disableNotification = true
-    ): void {
-        $constructorArgs = [
-            $apiKey,
-            $channel,
-            Logger::DEBUG,
-            true,
-            $parseMode,
-            $disableWebPagePreview,
-            $disableNotification,
-        ];
+    public function testTooLongMessageHandling(string $strategy, int $messageLength, int $expectedRequestsCounts)
+    {
+        $this->createHandler(
+            'testKey',
+            'testChannel',
+            'HTML',
+            false,
+            false,
+            $strategy,
+            $expectedRequestsCounts
+        );
 
-        $this->handler = $this->getMockBuilder(TelegramBotHandler::class)
-            ->setConstructorArgs($constructorArgs)
-            ->setMethods(['send'])
-            ->getMock();
+        $message = str_repeat('A', $messageLength);
 
-        $this->handler->expects($this->atLeast(1))
-            ->method('send');
+        $this->handler->handle(
+            $this->getRecord(
+                Logger::WARNING,
+                $message
+            )
+        );
     }
 
     public function testSetInvalidParseMode(): void
@@ -73,5 +71,55 @@ class TelegramBotHandlerTest extends TestCase
     {
         $handler = new TelegramBotHandler('testKey', 'testChannel');
         $handler->setParseMode('HTML');
+    }
+
+    /** @return Generator */
+    public function tooLongMessageHandlingProvider(): Generator
+    {
+        yield 'message-not-long' => [TelegramBotHandler::SPLIT_TOO_LONG_MESSAGE_INTO_MULTIPLE_MESSAGES, 4095, 1];
+        yield 'split-long-message-into-multiple-messages' => [
+            TelegramBotHandler::SPLIT_TOO_LONG_MESSAGE_INTO_MULTIPLE_MESSAGES,
+            8192,
+            2,
+        ];
+        yield 'trim-too-long-message' => [TelegramBotHandler::TRIM_TOO_LONG_MESSAGE, 8192, 1];
+
+        // because we can not mock curl request we can't expect exception here, but at least we should be sure that
+        // method send() be called once.
+        yield 'do-nothing-with-long-message' => [TelegramBotHandler::DO_NOTHING_WITH_TOO_LONG_MESSAGE, 8192, 1];
+        yield 'unexpected-strategy-provided' => ['bla-bla', 8192, 1];
+    }
+
+    /**
+     * @param string $apiKey
+     * @param string $channel
+     */
+    private function createHandler(
+        string $apiKey = 'testKey',
+        string $channel = 'testChannel',
+        string $parseMode = 'Markdown',
+        bool $disableWebPagePreview = false,
+        bool $disableNotification = true,
+        string $tooLongMessageHandlingStrategy = TelegramBotHandler::DO_NOTHING_WITH_TOO_LONG_MESSAGE,
+        int $methodSendCallsCount = 1
+    ): void {
+        $constructorArgs = [
+            $apiKey,
+            $channel,
+            Logger::DEBUG,
+            true,
+            $parseMode,
+            $disableWebPagePreview,
+            $disableNotification,
+            $tooLongMessageHandlingStrategy,
+        ];
+
+        $this->handler = $this->getMockBuilder(TelegramBotHandler::class)
+            ->setConstructorArgs($constructorArgs)
+            ->setMethods(['send'])
+            ->getMock();
+
+        $this->handler->expects($this->atLeast($methodSendCallsCount))
+            ->method('send');
     }
 }

f6572a3d2814e554dae0683bbdec08b1e8d684c4
Alexandr Mazur <amazur@xmlshop.com>
2021-03-26 12:38:36 +0200

    Added pre-handling too long messages before requesting Telegram API.

--- src/Monolog/Handler/TelegramBotHandler.php
+++ src/Monolog/Handler/TelegramBotHandler.php
@@ -30,6 +30,11 @@ use Monolog\Logger;
  */
 class TelegramBotHandler extends AbstractProcessingHandler
 {
+    public const MESSAGE_LENGTH_LIMIT = 4096;
+    public const TRIM_TOO_LONG_MESSAGE = 'trim';
+    public const SPLIT_TOO_LONG_MESSAGE_INTO_MULTIPLE_MESSAGES = 'split';
+    public const DO_NOTHING_WITH_TOO_LONG_MESSAGE = 'nothing';
+
     private const BOT_API = 'https://api.telegram.org/bot';
 
     /**
@@ -76,8 +81,15 @@ class TelegramBotHandler extends AbstractProcessingHandler
     private $disableNotification;
 
     /**
+     * Describes the strategy by which too long messages will be processed.
+     * @var string
+     */
+    private $howToHandleTooLongMessage;
+
+    /**
      * @param string $apiKey  Telegram bot access token provided by BotFather
      * @param string $channel Telegram channel name
+     * @param string $handleTooLongMessage Describes what to do with too long messages
      * @inheritDoc
      */
     public function __construct(
@@ -87,7 +99,8 @@ class TelegramBotHandler extends AbstractProcessingHandler
         bool $bubble = true,
         string $parseMode = null,
         bool $disableWebPagePreview = null,
-        bool $disableNotification = null
+        bool $disableNotification = null,
+        string $handleTooLongMessage = self::DO_NOTHING_WITH_TOO_LONG_MESSAGE
     ) {
         parent::__construct($level, $bubble);
 
@@ -96,6 +109,7 @@ class TelegramBotHandler extends AbstractProcessingHandler
         $this->setParseMode($parseMode);
         $this->disableWebPagePreview($disableWebPagePreview);
         $this->disableNotification($disableNotification);
+        $this->howToHandleTooLongMessage = $handleTooLongMessage;
     }
 
     public function setParseMode(string $parseMode = null): self
@@ -158,24 +172,94 @@ class TelegramBotHandler extends AbstractProcessingHandler
      */
     protected function send(string $message): void
     {
+        $messages = $this->handleTooLongMessage($message);
+
+        foreach ($messages as $message) {
+            $ch = $this->prepareCurlHandle($message);
+            $result = Curl\Util::execute($ch);
+            $result = json_decode($result, true);
+
+            if ($result['ok'] === false) {
+                throw new RuntimeException('Telegram API error. Description: ' . $result['description']);
+            }
+        }
+    }
+
+    /**
+     * Create a curl resource for further requesting Telegram API.
+     *
+     * @param string $message
+     * @return resource
+     */
+    protected function prepareCurlHandle(string $message)
+    {
         $ch = curl_init();
         $url = self::BOT_API . $this->apiKey . '/SendMessage';
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-            'text' => $message,
-            'chat_id' => $this->channel,
-            'parse_mode' => $this->parseMode,
-            'disable_web_page_preview' => $this->disableWebPagePreview,
-            'disable_notification' => $this->disableNotification,
-        ]));
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            http_build_query(
+                [
+                    'text' => $message,
+                    'chat_id' => $this->channel,
+                    'parse_mode' => $this->parseMode,
+                    'disable_web_page_preview' => $this->disableWebPagePreview,
+                    'disable_notification' => $this->disableNotification,
+                ]
+            )
+        );
 
-        $result = Curl\Util::execute($ch);
-        $result = json_decode($result, true);
+        return $ch;
+    }
 
-        if ($result['ok'] === false) {
-            throw new RuntimeException('Telegram API error. Description: ' . $result['description']);
+    /**
+     * Handle too long message.
+     *
+     * @return string[]
+     */
+    protected function handleTooLongMessage(string $message): array
+    {
+        // nothing to do with the message if message is inside in the limit
+        if (function_exists('mb_strlen') && mb_strlen($message) <= self::MESSAGE_LENGTH_LIMIT) {
+            return [$message];
+        } elseif (strlen($message) <= self::MESSAGE_LENGTH_LIMIT) {
+            return [$message];
+        }
+
+        // do something with the message if the message is outside of the limit.
+        switch ($this->howToHandleTooLongMessage) {
+            case self::TRIM_TOO_LONG_MESSAGE:
+                // the whole message will be trimmed to the maximum message length.
+                if (function_exists('mb_substr')) {
+                    return [mb_substr($message, 0, self::MESSAGE_LENGTH_LIMIT - 3) . '...'];
+                }
+                return [substr($message, 0, self::MESSAGE_LENGTH_LIMIT - 3) . '...'];
+            case self::SPLIT_TOO_LONG_MESSAGE_INTO_MULTIPLE_MESSAGES:
+                // the message will be chunked into separate messages with the maximum length limit.
+                if (function_exists('mb_str_split')) {
+                    $splittedMessages = mb_str_split($message, self::MESSAGE_LENGTH_LIMIT - 3);
+                    if ($splittedMessages === false) {
+                        throw new RuntimeException('Unable to split too long message');
+                    }
+
+                    $lastChunk = end($splittedMessages);
+                    foreach ($splittedMessages as &$splittedMessage) {
+                        if ($lastChunk === $splittedMessage) {
+                            break;
+                        }
+
+                        $splittedMessage .= '...';
+                    }
+                }
+
+                return str_split($message, self::MESSAGE_LENGTH_LIMIT);
+            // It's most likely cause 'message is too long' exception from Telegram API.
+            case self::DO_NOTHING_WITH_TOO_LONG_MESSAGE:
+            default:
+                return [$message];
         }
     }
 }

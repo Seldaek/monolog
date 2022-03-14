cb3675ee15cc0157c1694b8cac1af7ad436b31c7
Jordi Boggiano <j.boggiano@seld.be>
2022-03-14 13:51:26 +0100

    Try fixing ci

--- tests/Monolog/Handler/StreamHandlerTest.php
+++ tests/Monolog/Handler/StreamHandlerTest.php
@@ -145,10 +145,7 @@ class StreamHandlerTest extends TestCase
     public function testWriteInvalidResource()
     {
         $this->expectException(\UnexpectedValueException::class);
-        $this->expectExceptionMessage('The stream or file "bogus://url" could not be opened in append mode: Failed to open stream: No such file or directory
-The exception occurred while attempting to log: test
-Context: {"foo":"bar"}
-Extra: [1,2,3]');
+        $this->expectExceptionMessage('The stream or file "bogus://url" could not be opened in append mode: Failed to open stream: No such file or directory'."\n".'The exception occurred while attempting to log: test'."\n".'Context: {"foo":"bar"}'."\n".'Extra: [1,2,3]');
 
         $handler = new StreamHandler('bogus://url');
         $record = $this->getRecord();

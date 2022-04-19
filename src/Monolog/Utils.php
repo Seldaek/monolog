e4bb5c5cf33ba0152fde723ca94f194f9cd517fc
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2022-04-19 12:51:29 -0700

    Replace switch with match (#1655)
    The `match` expression is available as of PHP 8.0 and provides a more
    intuitive interface than `switch` when a single value is returned.
    
    Note that it performs an identity check (===) between the value and
    case instead of a weak equality check (==).
    
    https://www.php.net/manual/en/control-structures.match.php

--- src/Monolog/Utils.php
+++ src/Monolog/Utils.php
@@ -168,22 +168,13 @@ final class Utils
      */
     private static function throwEncodeError(int $code, $data): never
     {
-        switch ($code) {
-            case JSON_ERROR_DEPTH:
-                $msg = 'Maximum stack depth exceeded';
-                break;
-            case JSON_ERROR_STATE_MISMATCH:
-                $msg = 'Underflow or the modes mismatch';
-                break;
-            case JSON_ERROR_CTRL_CHAR:
-                $msg = 'Unexpected control character found';
-                break;
-            case JSON_ERROR_UTF8:
-                $msg = 'Malformed UTF-8 characters, possibly incorrectly encoded';
-                break;
-            default:
-                $msg = 'Unknown error';
-        }
+        $msg = match ($code) {
+            JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH => 'Underflow or the modes mismatch',
+            JSON_ERROR_CTRL_CHAR => 'Unexpected control character found',
+            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded',
+            default => 'Unknown error',
+        };
 
         throw new \RuntimeException('JSON encoding failed: '.$msg.'. Encoding: '.var_export($data, true));
     }

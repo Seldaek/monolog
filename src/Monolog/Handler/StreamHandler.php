c33fe008fe8c3dbf186b90bd19cd65dc882974ca
Kirill Voronin <kirill.voronin@paxful.com>
2022-06-09 10:44:34 +0300

    resource lock is the same and depends on only the directory path

--- src/Monolog/Handler/StreamHandler.php
+++ src/Monolog/Handler/StreamHandler.php
@@ -192,7 +192,7 @@ class StreamHandler extends AbstractProcessingHandler
 
         $dir = $this->getDirFromStream($url);
 
-        $this->dirLock = tempnam(sys_get_temp_dir(), md5($dir . time()));
+        $this->dirLock = tempnam(sys_get_temp_dir(), md5($dir));
 
         $resourceLock = fopen($this->dirLock, "r+");
 

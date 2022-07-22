Seldaek/monolog

commit a7e5beda577523edfc04ad12433889daccc5c4e9

finishing Syslog formatter (#1689)
* feat: Syslog formatter

* feat: Syslog formatter, app name

* update syslogFormatter

* Remove legacy syntax

Co-authored-by: Dalibor Karlović <dalibor.karlovic@sigwin.hr>
Co-authored-by: Renat Gabdullin <renatobyj@gmail.com>

src/Monolog/Formatter/SyslogFormatter.php
@@ -1,0 +1,67 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Monolog\Formatter;
+
+use Monolog\Handler\Syslog\SyslogUtils;
+use Monolog\Level;
+use Monolog\LogRecord;
+
+/**
+ * Serializes a log message according to RFC 5424
+ *
+ * @author Dalibor Karlović <dalibor.karlovic@sigwin.hr>
+ * @author Renat Gabdullin <renatobyj@gmail.com>
+ */
+class SyslogFormatter extends LineFormatter
+{
+    private const SYSLOG_FACILITY_USER = 1;
+    private const FORMAT = "<%extra.priority%>1 %datetime% %extra.hostname% %extra.app-name% %extra.procid% %channel% %extra.structured-data% %level_name%: %message% %context% %extra%\n";
+    private const NILVALUE = '-';
+
+    private string $hostname;
+    private int $procid;
+
+    public function __construct(private string $applicationName = self::NILVALUE)
+    {
+        parent::__construct(self::FORMAT, 'Y-m-d\TH:i:s.uP', true, true);
+        $this->hostname = (string) gethostname();
+        $this->procid = (int) getmypid();
+    }
+
+    public function format(LogRecord $record): string
+    {
+        $record->extra = $this->formatExtra($record);
+
+        return parent::format($record);
+    }
+
+    /**
+     * @param LogRecord $record
+     * @return array<string, mixed>
+     */
+    private function formatExtra(LogRecord $record): array
+    {
+        $extra = $record->extra;
+        $extra['app-name'] = $this->applicationName;
+        $extra['hostname'] = $this->hostname;
+        $extra['procid'] = $this->procid;
+        $extra['priority'] = self::calculatePriority($record->level);
+        $extra['structured-data'] = self::NILVALUE;
+        
+        return $extra;
+    }
+
+    private static function calculatePriority(Level $level): int
+    {
+        return (self::SYSLOG_FACILITY_USER * 8) + SyslogUtils::toSyslogPriority($level);
+    }
+}

src/Monolog/Handler/AbstractSyslogHandler.php
@@ -23,23 +23,6 @@ abstract class AbstractSyslogHandler extends AbstractProcessingHandler
     protected int $facility;
 
     /**
-     * Translates Monolog log levels to syslog log priorities.
-     */
-    protected function toSyslogPriority(Level $level): int
-    {
-        return match ($level) {
-            Level::Debug     => \LOG_DEBUG,
-            Level::Info      => \LOG_INFO,
-            Level::Notice    => \LOG_NOTICE,
-            Level::Warning   => \LOG_WARNING,
-            Level::Error     => \LOG_ERR,
-            Level::Critical  => \LOG_CRIT,
-            Level::Alert     => \LOG_ALERT,
-            Level::Emergency => \LOG_EMERG,
-        };
-    }
-
-    /**
      * List of valid log facility names.
      * @var array<string, int>
      */

src/Monolog/Handler/Syslog/SyslogUtils.php
@@ -1,0 +1,22 @@
+<?php
+
+namespace Monolog\Handler\Syslog;
+
+use Monolog\Level;
+
+class SyslogUtils
+{
+    public static function toSyslogPriority(Level $level): int
+    {
+        return match ($level) {
+            Level::Debug     => \LOG_DEBUG,
+            Level::Info      => \LOG_INFO,
+            Level::Notice    => \LOG_NOTICE,
+            Level::Warning   => \LOG_WARNING,
+            Level::Error     => \LOG_ERR,
+            Level::Critical  => \LOG_CRIT,
+            Level::Alert     => \LOG_ALERT,
+            Level::Emergency => \LOG_EMERG,
+        };
+    }
+}

src/Monolog/Handler/SyslogHandler.php
@@ -11,6 +11,7 @@
 
 namespace Monolog\Handler;
 
+use Monolog\Handler\Syslog\SyslogUtils;
 use Monolog\Level;
 use Monolog\Utils;
 use Monolog\LogRecord;
@@ -61,6 +62,6 @@ class SyslogHandler extends AbstractSyslogHandler
         if (!openlog($this->ident, $this->logopts, $this->facility)) {
             throw new \LogicException('Can\'t open syslog for ident "'.$this->ident.'" and facility "'.$this->facility.'"' . Utils::getRecordMessageForException($record));
         }
-        syslog($this->toSyslogPriority($record->level), (string) $record->formatted);
+        syslog(SyslogUtils::toSyslogPriority($record->level), (string) $record->formatted);
     }
 }

src/Monolog/Handler/SyslogUdpHandler.php
@@ -13,7 +13,8 @@ namespace Monolog\Handler;
 
 use DateTimeInterface;
 use Monolog\Level;
-use Monolog\Handler\SyslogUdp\UdpSocket;
+use Monolog\Handler\Syslog\SyslogUdp\UdpSocket;
+use Monolog\Handler\Syslog\SyslogUtils;
 use Monolog\Utils;
 use Monolog\LogRecord;
 
@@ -70,7 +71,7 @@ class SyslogUdpHandler extends AbstractSyslogHandler
     {
         $lines = $this->splitMessageIntoLines($record->formatted);
 
-        $header = $this->makeCommonSyslogHeader($this->toSyslogPriority($record->level), $record->datetime);
+        $header = $this->makeCommonSyslogHeader(SyslogUtils::toSyslogPriority($record->level), $record->datetime);
 
         foreach ($lines as $line) {
             $this->socket->write($line, $header);

tests/Monolog/Handler/SyslogUdpHandlerTest.php
@@ -35,7 +35,7 @@ class SyslogUdpHandlerTest extends TestCase
         $handler->setFormatter(new \Monolog\Formatter\ChromePHPFormatter());
 
         $time = '2014-01-07T12:34:56+00:00';
-        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
             ->onlyMethods(['write'])
             ->setConstructorArgs(['lol'])
             ->getMock();
@@ -56,7 +56,7 @@ class SyslogUdpHandlerTest extends TestCase
         $handler = new SyslogUdpHandler("127.0.0.1", 514, "authpriv");
         $handler->setFormatter($this->getIdentityFormatter());
 
-        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
             ->onlyMethods(['write'])
             ->setConstructorArgs(['lol'])
             ->getMock();
@@ -81,7 +81,7 @@ class SyslogUdpHandlerTest extends TestCase
 
         $handler->setFormatter(new \Monolog\Formatter\ChromePHPFormatter());
 
-        $socket = $this->getMockBuilder('\Monolog\Handler\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('\Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
             ->setConstructorArgs(['lol', 999])
             ->onlyMethods(['write'])
             ->getMock();

tests/Monolog/Handler/UdpSocketTest.php
@@ -12,7 +12,7 @@
 namespace Monolog\Handler;
 
 use Monolog\Test\TestCase;
-use Monolog\Handler\SyslogUdp\UdpSocket;
+use Monolog\Handler\Syslog\SyslogUdp\UdpSocket;
 
 /**
  * @requires extension sockets
@@ -21,7 +21,7 @@ class UdpSocketTest extends TestCase
 {
     public function testWeDoNotTruncateShortMessages()
     {
-        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
             ->onlyMethods(['send'])
             ->setConstructorArgs(['lol'])
             ->getMock();
@@ -35,7 +35,7 @@ class UdpSocketTest extends TestCase
 
     public function testLongMessagesAreTruncated()
     {
-        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
             ->onlyMethods(['send'])
             ->setConstructorArgs(['lol'])
             ->getMock();

src/Monolog/Handler/Syslog/SyslogUdp/UdpSocket.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Monolog\Handler\SyslogUdp;
+namespace Monolog\Handler\Syslog\SyslogUdp;
 
 use Monolog\Utils;
 use Socket;

commit f7dfa00f084db62b1dec3b9ef683be686cfa31d7

Add Level::toRFC5424Level method and restore BC to Syslog handlers (broken in #1689)

src/Monolog/Formatter/SyslogFormatter.php
@@ -56,12 +56,12 @@ class SyslogFormatter extends LineFormatter
         $extra['procid'] = $this->procid;
         $extra['priority'] = self::calculatePriority($record->level);
         $extra['structured-data'] = self::NILVALUE;
-        
+
         return $extra;
     }
 
     private static function calculatePriority(Level $level): int
     {
-        return (self::SYSLOG_FACILITY_USER * 8) + SyslogUtils::toSyslogPriority($level);
+        return (self::SYSLOG_FACILITY_USER * 8) + $level->toRFC5424Level();
     }
 }

src/Monolog/Handler/AbstractSyslogHandler.php
@@ -41,6 +41,14 @@ abstract class AbstractSyslogHandler extends AbstractProcessingHandler
     ];
 
     /**
+     * Translates Monolog log levels to syslog log priorities.
+     */
+    protected function toSyslogPriority(Level $level): int
+    {
+        return $level->toRFC5424Level();
+    }
+
+    /**
      * @param string|int $facility Either one of the names of the keys in $this->facilities, or a LOG_* facility constant
      */
     public function __construct(string|int $facility = \LOG_USER, int|string|Level $level = Level::Debug, bool $bubble = true)

src/Monolog/Handler/Syslog/SyslogUtils.php
@@ -1,22 +1,0 @@
-<?php
-
-namespace Monolog\Handler\Syslog;
-
-use Monolog\Level;
-
-class SyslogUtils
-{
-    public static function toSyslogPriority(Level $level): int
-    {
-        return match ($level) {
-            Level::Debug     => \LOG_DEBUG,
-            Level::Info      => \LOG_INFO,
-            Level::Notice    => \LOG_NOTICE,
-            Level::Warning   => \LOG_WARNING,
-            Level::Error     => \LOG_ERR,
-            Level::Critical  => \LOG_CRIT,
-            Level::Alert     => \LOG_ALERT,
-            Level::Emergency => \LOG_EMERG,
-        };
-    }
-}

src/Monolog/Handler/SyslogHandler.php
@@ -62,6 +62,6 @@ class SyslogHandler extends AbstractSyslogHandler
         if (!openlog($this->ident, $this->logopts, $this->facility)) {
             throw new \LogicException('Can\'t open syslog for ident "'.$this->ident.'" and facility "'.$this->facility.'"' . Utils::getRecordMessageForException($record));
         }
-        syslog(SyslogUtils::toSyslogPriority($record->level), (string) $record->formatted);
+        syslog($this->toSyslogPriority($record->level), (string) $record->formatted);
     }
 }

src/Monolog/Handler/SyslogUdp/UdpSocket.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Monolog\Handler\Syslog\SyslogUdp;
+namespace Monolog\Handler\SyslogUdp;
 
 use Monolog\Utils;
 use Socket;

src/Monolog/Handler/SyslogUdpHandler.php
@@ -12,11 +12,11 @@
 namespace Monolog\Handler;
 
 use DateTimeInterface;
-use Monolog\Level;
-use Monolog\Handler\Syslog\SyslogUdp\UdpSocket;
 use Monolog\Handler\Syslog\SyslogUtils;
-use Monolog\Utils;
+use Monolog\Handler\SyslogUdp\UdpSocket;
+use Monolog\Level;
 use Monolog\LogRecord;
+use Monolog\Utils;
 
 /**
  * A Handler for logging to a remote syslogd server.
@@ -71,7 +71,7 @@ class SyslogUdpHandler extends AbstractSyslogHandler
     {
         $lines = $this->splitMessageIntoLines($record->formatted);
 
-        $header = $this->makeCommonSyslogHeader(SyslogUtils::toSyslogPriority($record->level), $record->datetime);
+        $header = $this->makeCommonSyslogHeader($this->toSyslogPriority($record->level), $record->datetime);
 
         foreach ($lines as $line) {
             $this->socket->write($line, $header);

src/Monolog/Level.php
@@ -23,6 +23,7 @@ use Psr\Log\LogLevel;
  *
  * - Use ->getName() to get the standard Monolog name which is full uppercased (e.g. "DEBUG")
  * - Use ->toPsrLogLevel() to get the standard PSR-3 name which is full lowercased (e.g. "debug")
+ * - Use ->toRFC5424Level() to get the standard RFC 5424 value (e.g. 7 for debug, 0 for emergency)
  * - Use ->name to get the enum case's name which is capitalized (e.g. "Debug")
  *
  * To get the value for filtering, if the includes/isLowerThan/isHigherThan methods are
@@ -147,6 +148,8 @@ enum Level: int
     }
 
     /**
+     * Returns the PSR-3 level matching this instance
+     *
      * @phpstan-return \Psr\Log\LogLevel::*
      */
     public function toPsrLogLevel(): string
@@ -160,6 +163,25 @@ enum Level: int
             self::Critical => LogLevel::CRITICAL,
             self::Alert => LogLevel::ALERT,
             self::Emergency => LogLevel::EMERGENCY,
+        };
+    }
+
+    /**
+     * Returns the RFC 5424 level matching this instance
+     *
+     * @phpstan-return int<0, 7>
+     */
+    public function toRFC5424Level(): int
+    {
+        return match ($this) {
+            self::Debug => 7,
+            self::Info => 6,
+            self::Notice => 5,
+            self::Warning => 4,
+            self::Error => 3,
+            self::Critical => 2,
+            self::Alert => 1,
+            self::Emergency => 0,
         };
     }
 

tests/Monolog/Handler/SyslogUdpHandlerTest.php
@@ -35,7 +35,7 @@ class SyslogUdpHandlerTest extends TestCase
         $handler->setFormatter(new \Monolog\Formatter\ChromePHPFormatter());
 
         $time = '2014-01-07T12:34:56+00:00';
-        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
             ->onlyMethods(['write'])
             ->setConstructorArgs(['lol'])
             ->getMock();
@@ -56,7 +56,7 @@ class SyslogUdpHandlerTest extends TestCase
         $handler = new SyslogUdpHandler("127.0.0.1", 514, "authpriv");
         $handler->setFormatter($this->getIdentityFormatter());
 
-        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
             ->onlyMethods(['write'])
             ->setConstructorArgs(['lol'])
             ->getMock();
@@ -81,7 +81,7 @@ class SyslogUdpHandlerTest extends TestCase
 
         $handler->setFormatter(new \Monolog\Formatter\ChromePHPFormatter());
 
-        $socket = $this->getMockBuilder('\Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('\Monolog\Handler\SyslogUdp\UdpSocket')
             ->setConstructorArgs(['lol', 999])
             ->onlyMethods(['write'])
             ->getMock();

tests/Monolog/Handler/UdpSocketTest.php
@@ -11,8 +11,8 @@
 
 namespace Monolog\Handler;
 
+use Monolog\Handler\SyslogUdp\UdpSocket;
 use Monolog\Test\TestCase;
-use Monolog\Handler\Syslog\SyslogUdp\UdpSocket;
 
 /**
  * @requires extension sockets
@@ -21,7 +21,7 @@ class UdpSocketTest extends TestCase
 {
     public function testWeDoNotTruncateShortMessages()
     {
-        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
             ->onlyMethods(['send'])
             ->setConstructorArgs(['lol'])
             ->getMock();
@@ -35,7 +35,7 @@ class UdpSocketTest extends TestCase
 
     public function testLongMessagesAreTruncated()
     {
-        $socket = $this->getMockBuilder('Monolog\Handler\Syslog\SyslogUdp\UdpSocket')
+        $socket = $this->getMockBuilder('Monolog\Handler\SyslogUdp\UdpSocket')
             ->onlyMethods(['send'])
             ->setConstructorArgs(['lol'])
             ->getMock();
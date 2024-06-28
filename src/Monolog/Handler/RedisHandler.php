f836e01659d4e94099c49c860eb5e7f030ec07ab
javaDeveloperKid <25783196+javaDeveloperKid@users.noreply.github.com>
2024-06-28 11:06:36 +0200

    Update example in RedisHandler (#1894)

--- src/Monolog/Handler/RedisHandler.php
+++ src/Monolog/Handler/RedisHandler.php
@@ -24,7 +24,7 @@ use Redis;
  * usage example:
  *
  *   $log = new Logger('application');
- *   $redis = new RedisHandler(new Predis\Client("tcp://localhost:6379"), "logs", "prod");
+ *   $redis = new RedisHandler(new Predis\Client("tcp://localhost:6379"), "logs");
  *   $log->pushHandler($redis);
  *
  * @author Thomas Tourlourat <thomas@tourlourat.com>

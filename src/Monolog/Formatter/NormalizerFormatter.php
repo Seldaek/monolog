e94000419394ff1bec801dad310432228f9fc19c
Jordi Boggiano <j.boggiano@seld.be>
2024-11-17 13:30:33 +0100

    Fix normalizeException types, fixes #1924

--- src/Monolog/Formatter/NormalizerFormatter.php
+++ src/Monolog/Formatter/NormalizerFormatter.php
@@ -248,7 +248,7 @@ class NormalizerFormatter implements FormatterInterface
     }
 
     /**
-     * @return array<string, string|int|array<string|int|array<string>>>|string
+     * @return array<array-key, string|int|array<string|int|array<string>>>
      */
     protected function normalizeException(Throwable $e, int $depth = 0)
     {

90388b3f0d21d2b9e5604ac0d99538beb2ca466e
rajmundtoth0 <rajm.toth@gmail.com>
2025-03-10 10:19:38 +0100

    SendGrid V3 API

--- src/Monolog/Handler/SendGridHandler.php
+++ src/Monolog/Handler/SendGridHandler.php
@@ -20,81 +20,55 @@ use Monolog\Level;
  */
 class SendGridHandler extends MailHandler
 {
+    private const string CONTENT_TYPE = 'Content-Type: application/json';
     /**
-     * The SendGrid API User
-     */
-    protected string $apiUser;
-
-    /**
-     * The SendGrid API Key
-     */
-    protected string $apiKey;
-
-    /**
-     * The email addresses to which the message will be sent
-     */
-    protected string $from;
-
-    /**
-     * The email addresses to which the message will be sent
-     * @var string[]
-     */
-    protected array $to;
-
-    /**
-     * The subject of the email
-     */
-    protected string $subject;
-
-    /**
-     * @param string          $apiUser The SendGrid API User
-     * @param string          $apiKey  The SendGrid API Key
-     * @param string          $from    The sender of the email
-     * @param string|string[] $to      The recipients of the email
-     * @param string          $subject The subject of the mail
-     *
      * @throws MissingExtensionException If the curl extension is missing
      */
-    public function __construct(string $apiUser, string $apiKey, string $from, string|array $to, string $subject, int|string|Level $level = Level::Error, bool $bubble = true)
+    public function __construct(
+        private readonly string $apiKey,
+        private readonly string $from,
+        private readonly array $to,
+        private readonly string $subject,
+        int|string|Level $level = Level::Error,
+        protected bool $bubble = true,
+        private readonly string $sendGridApiUrl = 'https://api.sendgrid.com/v3/mail/send',
+        )
     {
         if (!\extension_loaded('curl')) {
             throw new MissingExtensionException('The curl extension is needed to use the SendGridHandler');
         }
 
         parent::__construct($level, $bubble);
-        $this->apiUser = $apiUser;
-        $this->apiKey = $apiKey;
-        $this->from = $from;
-        $this->to = (array) $to;
-        $this->subject = $subject;
     }
 
-    /**
-     * @inheritDoc
-     */
     protected function send(string $content, array $records): void
     {
-        $message = [];
-        $message['api_user'] = $this->apiUser;
-        $message['api_key'] = $this->apiKey;
-        $message['from'] = $this->from;
+        $body = [];
+        $body['personalizations'] = [];
+        $body['from']['email'] = $this->from;
         foreach ($this->to as $recipient) {
-            $message['to[]'] = $recipient;
+            $body['personalizations'][]['to'][]['email'] = $recipient;
         }
-        $message['subject'] = $this->subject;
-        $message['date'] = date('r');
+        $body['subject'] = $this->subject;
 
         if ($this->isHtmlBody($content)) {
-            $message['html'] = $content;
+            $body['content'][] = [
+                'type' => 'text/html',
+                'value' => $content,
+            ];
         } else {
-            $message['text'] = $content;
+            $body['content'][] = [
+                'type' => 'text/plain',
+                'value' => $content,
+            ];
         }
-
+        $authorization = "Authorization: Bearer {$this->apiKey}";
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api.sendgrid.com/api/mail.send.json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [self::CONTENT_TYPE , $authorization]);
+        curl_setopt($ch, CURLOPT_URL, $this->sendGridApiUrl);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($message));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
         Curl\Util::execute($ch, 2);
     }
 }

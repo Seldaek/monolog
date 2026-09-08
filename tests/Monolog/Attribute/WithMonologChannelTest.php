db9ea92b1180ab9bf0b00c0974e097212a634216
Grégoire Pineau <lyrixx@lyrixx.info>
2026-09-08 22:37:21 +0200

    Allow WithMonologChannel to target a constructor/method parameter (#2068)
    Widens the attribute to TARGET_CLASS | TARGET_PARAMETER so integrators
    can bind a logger channel to a single constructor/method argument
    instead of the whole class, which is needed to inject loggers for
    several different channels into the same service.

--- tests/Monolog/Attribute/WithMonologChannelTest.php
+++ tests/Monolog/Attribute/WithMonologChannelTest.php
@@ -18,4 +18,22 @@ class WithMonologChannelTest extends \Monolog\Test\MonologTestCase
         $attribute = new WithMonologChannel('fixture');
         $this->assertSame('fixture', $attribute->channel);
     }
+
+    public function testOnConstructorParameter(): void
+    {
+        $parameter = new \ReflectionParameter([WithMonologChannelOnParameterFixture::class, '__construct'], 'logger');
+        $attributes = $parameter->getAttributes(WithMonologChannel::class);
+
+        $this->assertCount(1, $attributes);
+        $this->assertSame('fixture', $attributes[0]->newInstance()->channel);
+    }
+}
+
+final class WithMonologChannelOnParameterFixture
+{
+    public function __construct(
+        #[WithMonologChannel('fixture')]
+        public readonly \stdClass $logger,
+    ) {
+    }
 }
